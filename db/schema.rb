e71b6f809635c8f5b8b403c221f95a4561d2f07e
Elhambasir <elham1378basir@gmail.com>
2023-08-25 14:08:02 +0430

    Update schema.rb using running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_043832) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_090648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "photo", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
