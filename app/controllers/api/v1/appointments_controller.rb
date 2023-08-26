f611c0ae9c61f2b229bb86fcdc901cdc497f6275
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-26 02:21:30 -0400

    Create crud for appointment controller

---
+++ app/controllers/api/v1/appointments_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::AppointmentsController < ApplicationController
+    before_action :set_appointment, only: [:show, :update, :destroy]
+
+  
+def index
+    @appointments = Appointment.all
+    render json: @appointments
+end
+
+def show
+    render json: @appointment
+end
+
+def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user.id
+    @appointment.doctor_id = Doctor.pluck(:id).sample  
+    if @appointment.save
+      render json: { message: 'Appointment created successfully' }, status: :created
+    else
+      render json: {errors: @appointment.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+def destroy
+  if @appointment.destroy
+    render json: { message: 'Appointment destroyed successfully' }
+  else
+    render json: { error: 'Failed to destroy appointment' }, status: :unprocessable_entity
+  end
+end
+
+private
+  
+
+def set_appointment
+  @appointment = Appointment.find_by(id:params[:id])
+
+  unless @appointment
+    render json: { error: 'Appointment not found' }, status: :not_found
+  end
+end
+
+
+def appointment_params
+       params.require(:appointment).permit(:date, :city)
+end
+
+end
+
+
+
+
+
