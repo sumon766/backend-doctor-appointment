01b6021d4c96c994f03d20ab60529840061203bc
James Serengia <jamesserengia@gmail.com>
2023-09-07 19:19:07 +0300

    Add config.assets.compile = false

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BackendDoctorAppointment
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.compile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
