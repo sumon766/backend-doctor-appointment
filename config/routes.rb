c304ccebf98baca9c23d7839b377a99fb58a54c5
James Serengia <jamesserengia@gmail.com>
2023-09-07 18:39:41 +0300

    Prepared project for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "doctors#index"
+  root "pages#index"
   namespace :api do
     namespace :v1 do
       resources :doctors
