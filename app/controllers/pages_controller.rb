6686e70a17c53b567e1de3f0b6ae341b04b13b73
James Serengia <jamesserengia@gmail.com>
2023-09-08 13:10:06 +0300

    Update render-build.sh

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    def index
-        render json: { status: 200, message: "Welcome to Doctors Appointment API" }
-    end
+  def index
+    render json: { status: 200, message: 'Welcome to Doctors Appointment API' }
+  end
 end
